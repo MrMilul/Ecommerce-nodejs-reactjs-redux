f36a2b510b9b0a6215e48705f48f8394bc914e17
MrMilul <miladmizani@yahoo.com>
2021-01-17 11:29:44 +0200

    improvment

--- backend/route/category.js
+++ backend/route/category.js
@@ -1,34 +1,9 @@
 const express = require('express');
 const router = express.Router()
-const mongoose = require('mongoose');
-const Category = require('../model/category')
+const categoryController = require('../controller/category')
 
-router.get('/', (req, res, next)=>{
-    Category.find()
-    .exec()
-    .then(result=>{
-        res.status(200).json({result})
-    })
-    .catch(err=>{
-        res.status(400).json({
-            message: err.message
-        })
-    })
-})
+router.get('/', categoryController.category_get_all)
 
-router.post('/', (req, res, next)=>{
-    const category = new Category({
-        category: req.body.category
-    }) 
-    category.save()
-    .then((result) =>{
-        res.status(201).json(result)
-    })
-    .catch(err =>{
-        res.status(400).json({
-            message: err.message,
-        })
-    })
-})
+router.post('/',categoryController.category_post )
 
 module.exports = router
