f3d57bae19022d28fedf94d97926d9cfccb81976
MrMilul <miladmizani@yahoo.com>
2021-04-29 20:19:04 +0300

    front end change

--- frontend/src/redux/actions/types.js
+++
@@ -1,8 +0,0 @@
-export const GET_CATEGOTY_DATA = "GET_CATEGOTY_DATA"
-
-export const get_categoty_data = data => {
-    return {
-        type: GET_CATEGOTY_DATA, 
-        payload: data
-    }
-}
--- frontend/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import  categoryReducer  from './category'
-
-
- const rootReducer =  combineReducers({
-    category: categoryReducer,
-}) 
-export default rootReducer
