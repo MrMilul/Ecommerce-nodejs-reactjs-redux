0f18697839a64f023b1caa95ef265b1b47b3b344
MrMilul <miladmizani@yahoo.com>
2021-05-06 20:52:21 +0300

    modified

--- server/models/products.js
+++ server/models/products.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: true}, 
-    price: {type: String, required: true}, 
-    img: {type: String}, 
-    desc: {type: String}, 
-    countInStock: {type: Number}, 
-    ingredientes: {type: String}, 
-    raiting: {type: Number}, 
-    numReview: {type: String}, 
-    category: {
-       type: mongoose.Schema.Types.ObjectID, 
-       ref: "Category"
-    }
-})
+  name: { type: String, required: true },
+  price: { type: String, required: true },
+  discount:{type: Number}, 
+  img: { type: String },
+  desc: { type: String },
+  countInStock: { type: Number },
+  ingredientes: { type: String },
+  raiting: { type: Number },
+  numReview: { type: String },
+  featured: {type: Boolean, default: false},
+  isVegan: {type: Boolean, default: false},
+});
 
-exports.module = mongoose.model('Product', productSchema)
-
+exports.module = mongoose.model("Product", productSchema);
