c9d704f1dec8ceed201edc51735e1ea4d55d3b4f
MrMilul <miladmizani@yahoo.com>
2021-05-09 08:38:34 +0300

    modified

--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -1,64 +1,61 @@
 const express = require("express");
-const productRout = express.Router();
+const productRoute = express.Router();
 const Product = require("../models/products.js");
-const multer = require('multer')
-const upload = multer({dest: './upload'})
+const multer = require("multer");
 
-const storage = ({
-  destination: (req, file, cb)=>{
-    cb(null, './upload/')
-  }, 
-  filename: (req, file, cb)=>{
-    cb(null, new Date().toIsoStrin()+file.filename)
-  }
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./upload/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
-})
-
-
-productRout.get("/", async (req, res) => {
+productRoute.get("/", async (req, res) => {
   try {
-    const product = await Product.find({});
-    if (product.length !== 0) {
-      res.status(200).json(product);
+    const products = await Product.find({});
+    if (products.length === 0) {
+      console.log("empty")
+      res.json({message:"the Product section is Empty"})
     } else {
-      res.status(200).json({ message: "The Product section is Empty" });
+      console.log("products")
     }
   } catch (error) {
-    res.json(error);
+    res.json(error)
   }
 });
 
-productRout.post("/", upload.single('img'), async (req, res) => {
-  console.log(req.file)
- 
-  // try {
-  //   const createProduct = await new Product({
-  //     name: req.body.name,
-  //     price:req.body,
-  //     img:req.file.path,
-  //     discount:req.body,
-  //     desc:req.body.desc,
-  //     countInStock: req.body.countInStock,
-  //     ingredientes:req.body.ingredientes, 
-  //     raiting:req.body,
-  //     numReview:req.body.numReview,
-  //     featured: req.body.featured,
-  //     isVegan: req.body.isVegan,
-  //   });
-      
-  //   createProduct.save();
-  //   res.status(201).json({
-  //     message: `The category-${createProduct.name}- is successfully created`,
-  //   });
-  // } catch (error) {
-  //   res.status(400).json(error);
-  // }
+productRoute.post("/", upload.single("img"), async (req, res) => {
+  try {
+    const createProduct = await new Product({
+      name: req.body.name,
+      price: req.body,
+      img: req.file.path,
+      discount: req.body,
+      desc: req.body.desc,
+      countInStock: req.body.countInStock,
+      ingredientes: req.body.ingredientes,
+      raiting: req.body,
+      numReview: req.body.numReview,
+      featured: req.body.featured,
+      isVegan: req.body.isVegan,
+    });
+
+    createProduct.save();
+    res.status(201).json({
+      message: `The category-${createProduct.name}- is successfully created`,
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-productRout.delete("/:id", async (req, res) => {
+productRoute.delete("/:id", async (req, res) => {
   const proId = req.params.id;
   try {
-    const proDeleted = await Product.remove({ _id: catId });
+    const proDeleted = await Product.remove({ _id: proId });
 
     res.status(201).json(proDeleted);
   } catch (error) {
@@ -66,5 +63,4 @@ productRout.delete("/:id", async (req, res) => {
   }
 });
 
-
-module.exports = productRout;
+module.exports = productRoute;
