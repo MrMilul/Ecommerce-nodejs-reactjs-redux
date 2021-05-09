e4275c7484c5e6844a243ac683618f0cbc260b13
MrMilul <miladmizani@yahoo.com>
2021-05-09 08:37:51 +0300

    added createUserIndex

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const DB = async() =>{
     try{
         await mongoose.connect ( process.env.DATABASE_LINK, 
-            {useNewUrlParser:true, useUnifiedTopology:true})
+            {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true})
     }catch(err){
         console.log("MongoDB Connection Fail")
         console.log(err.message)
