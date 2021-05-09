04c15a7d57591c395af96a96d95b86104f757cf6
MrMilul <miladmizani@yahoo.com>
2021-05-09 13:28:38 +0300

    user model is created

---
+++ server/models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const EmailValidation = (email) => {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const usersSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [EmailValidation, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Users", usersSchema);
