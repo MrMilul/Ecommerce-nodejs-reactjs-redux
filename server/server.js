6acd665476c0074e19ebc7273ca435b3365abe86
MrMilul <miladmizani@yahoo.com>
2021-05-10 20:25:23 +0300

    modified

--- server/server.js
+++ server/server.js
@@ -17,14 +17,17 @@ app.use('/upload/',express.static('upload'))
 //********************************** Constants *******************************************//
 const PORT = process.env.PORT || 5000;
 
+//********************************** DB *******************************************//
 const connectionToDB = require("./config/db.js");
 connectionToDB();
 //********************************** Routers *******************************************//
 const categoryRoute = require("./routers/categotyRouter.js");
-const productRoute = require("./routers/productRouter.js")
+const productRoute = require("./routers/productRouter.js");
+const usersRoute = require('./routers/usersRouter.js')
 
 app.use("/api/category", categoryRoute);
-app.use("/api/product", productRoute)
+app.use("/api/products", productRoute);
+app.use("/api/user", usersRoute)
 
 //********************************** Error Handleling ***********************************//
 app.use((req, res, next) => {
@@ -40,8 +43,6 @@ app.use((error, req, res, next) => {
     },
   });
 });
-
-
 
 app.listen(PORT, () => {
   console.log("Server Is Connected!");
