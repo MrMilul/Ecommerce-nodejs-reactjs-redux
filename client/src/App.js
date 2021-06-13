063c146bacb25d49521ec8617aa289df7be68b51
mrmilul <miladmizani@yahoo.com>
2021-06-13 11:41:08 +0300

    client changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import {
-  productData,
-  productDataTwo,
-  productDataThree,
-} from "./components/products/data.js";
+import logo from './logo.svg';
+import './App.css';
 
-import { GlobalStyle } from "./globalStyle";
-import Hero from "./components/Hero";
-import Products from "./components/products";
-import Feature from "./components/feature";
-import Footer from "./components/Footer";
-import Button from "./components/reuseable/button/Button.js";
 function App() {
   return (
-    <Router>
-      <GlobalStyle />
-      <Hero />
-      <Products
-        isProduct={true}
-        heading="Choose Your Favorite"
-        data={productData}
-        Vegan="Vegan"
-        Nonvegan="Non-vegan"
-        vitamins="Recommended"
-      />
-      <Feature />
-      <Products
-        isProduct={true}
-        heading="Self Service"
-        data={productDataTwo}
-        Vegan="Fruits"
-        Nonvegan="Carbs"
-        proteins="proteins"
-        oil="oil"
-        vitamins="vitamins"
-      />
-      <Products heading="Our Services" data={productDataThree} />
-      <Footer />
-      <Button   style="btn--outPrimary" size="btn--medium">
-        more info
-      </Button>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./upload/");
+    cb(null, "/upload/");
   },
   filename: function (req, file, cb) {
     cb(null, new Date().toISOString() + file.originalname);
