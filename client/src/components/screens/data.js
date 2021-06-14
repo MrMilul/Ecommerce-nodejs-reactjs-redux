e478388e85af056f3d58c109b3d4c2017b1419a8
mrmilul <miladmizani@yahoo.com>
2021-06-14 19:51:45 +0300

    json data is created to build Product screen

---
+++ client/src/components/screens/data.js
@@ -0,0 +1,47 @@
+export const products = [
+    {
+        id: "1",
+        name: "product 1", 
+        desc: "this is the first product",
+        price: "250",
+        img: "./upload/nike1.jpeg",
+        countInStock: "25",
+        raiting: 4,
+        numReview: "152",
+        featured: false
+    },
+    {
+        id: "2",
+        name: "product 2", 
+        desc: "this is the second product",
+        price: "250",
+        img: "./upload/nike2.jpeg",
+        countInStock: "25",
+        raiting: 4,
+        numReview: "152",
+        featured: false
+    },
+    {
+        id: "3",
+        name: "product 3", 
+        desc: "this is the third product",
+        price: "250",
+        img: "./upload/nike3.jpeg",
+        countInStock: "25",
+        raiting: 4,
+        numReview: "152",
+        featured: false
+    },
+    {
+        id: "4",
+        name: "product 4", 
+        desc: "this is the forth product",
+        price: "250",
+        img: "./upload/nike1.jpeg",
+        countInStock: "25",
+        raiting: 4,
+        numReview: "152",
+        featured: false
+    },
+]
+    
