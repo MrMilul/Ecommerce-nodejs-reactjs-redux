a65f8ee0e7818f6b739ea5bf8bea9df626be8d24
mrmilul <miladmizani@yahoo.com>
2021-06-15 19:03:24 +0300

    added data.js to the server to fetch data from the server

---
+++ server/data.js
@@ -0,0 +1,52 @@
+const data = {
+    products :  
+    [
+        {
+            id: "1",
+            name: "product 1", 
+            desc: "this is the first product",
+            price: "250",
+            img: "./upload/nike1.jpeg",
+            countInStock: "25",
+            raiting: 4.5,
+            numReview: "152",
+            featured: false
+        },
+        {
+            id: "2",
+            name: "product 2", 
+            desc: "this is the second product",
+            price: "250",
+            img: "./upload/nike2.jpeg",
+            countInStock: "25",
+            raiting: 3.26,
+            numReview: "152",
+            featured: false
+        },
+        {
+            id: "3",
+            name: "product 3", 
+            desc: "this is the third product",
+            price: "250",
+            img: "./upload/nike3.jpeg",
+            countInStock: "25",
+            raiting: 5,
+            numReview: "152",
+            featured: false
+        },
+        {
+            id: "4",
+            name: "product 4", 
+            desc: "this is the forth product",
+            price: "250",
+            img: "./upload/nike1.jpeg",
+            countInStock: "25",
+            raiting: 2.98,
+            numReview: "152",
+            featured: false
+        },
+    ]
+}
+
+
+module.exports = data
