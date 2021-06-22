4a09f552d0a61382b640df6bf4bcefc9eaa5fd6d
mrmilul <miladmizani@yahoo.com>
2021-06-22 19:08:17 +0300

    added select, handlers

--- client/src/components/screens/ProductDetail.js
+++ client/src/components/screens/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 
 import { ProductDetailAction } from '../../redux/actions/products'
@@ -9,29 +9,38 @@ import Raiting from '../constants/Raiting'
 
 
 export default function ProductDetail(props) {
-   
+   const [Qty, setQty] = useState(1)
+    console.log(Qty)
     const productId = props.match.params.id
     const dispatch = useDispatch()
     const productDet = useSelector(state => state.productDetail)
     const { loading, error, product } = productDet
+    const img = product.img
+    
+    const AddToCartHandler = ()=>{
+        props.history.push(`/cart/${product.id}?qty=${Qty}`)
+    }   
+    
     useEffect(()=>{
         dispatch(ProductDetailAction(productId))
-    }, [dispatch])
+    }, [dispatch, productId])
     return (
       <>
         {
             loading ? (<LoadingBox />) : error ? (<MessageBox status={"danger"} message={error} />)
                 :(
-                    <div className="container">
+                    <div className="container m-4">
                         <div className="row">
-                            <div className="col-6">
-                            <div className="card">
+
+                            <div className="col-lg-6 col-sm-12 mb-3">
+                                <div className="card">
                                     <div className="card-body">
-                                    <img src={product.img} className="img-fluid" alt={product.img}/>                                    </div>
-                                </div>
-                               
+                                        <img src={img} className="img" style={{width: "100%"}}alt={product.img}/>                                   
+                                    </div>
+                                 </div>
                             </div>
-                            <div className="col">
+
+                            <div className="col-lg-3 col-sm-12 mb-3">
                                 <div className="card">
                                     <div className="card-body">
                                         <h4>{product.name}</h4>
@@ -42,19 +51,28 @@ export default function ProductDetail(props) {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col">
-                            <div className="card" >
-                                    <div className="card-body">
-                                        Price: {product.price}$<br/>
 
+                            <div className="col-lg-3 col-sm-12 mb-3">
+                             <div className="card" >
+                                    <div className="card-body">
+                                       <ul>
+                                           <li>Price : {product.price}</li>
+                                           <li>
+                                                status: {''}
+                                                {product.countInStock > 0 ? 'In Stock' : 'Unavailabe'}
+                                           </li>
+                                           <li>
+                                               QTY: {product.countInStock > 0 && (
+                                                   <select value={Qty} onChange={(e=>setQty(e.target.value))}>
+                                                       {[...Array(product.countInStock).keys()].map((x)=>(
+                                                           <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                       ))}
+                                                   </select>
+                                               )}
+                                               <button onClick={AddToCartHandler} className={product.countInStock > 0 ? "btn btn-success btn-block": "btn btn-success btn-block disabled"}>Add to Cart</button>
+                                           </li>
+                                       </ul>
                                         
-                                        Qyt:
-                                        <select id="cars" className="mr-auto">
-                                        <option value="volvo">Volvo</option>
-                                        <option value="saab">Saab</option>
-                                        <option value="opel">Opel</option>
-                                        <option value="audi">Audi</option>
-                                        </select>
                                     </div>
                                     
                                 </div>
