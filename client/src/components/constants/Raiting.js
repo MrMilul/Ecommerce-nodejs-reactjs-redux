e7686baefc8442a7f9a00d0ab396b373d7738f09
mrmilul <miladmizani@yahoo.com>
2021-06-22 19:07:25 +0300

    deleted unneccessary values

--- client/src/components/constants/Raiting.js
+++ client/src/components/constants/Raiting.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Raiting = (props)=> {
-    const {raiting, numReview} = props
+    const {raiting} = props
     return (
         <div>
            <i style ={{color: "yellow"}}className= { raiting >= 1 ? "fas fa-star": raiting>=0.5 ? "fas fa-star-half"  : "far fa-star"} ></i>
@@ -10,10 +10,6 @@ const Raiting = (props)=> {
            <i style ={{color: "yellow"}} className= { raiting >= 4 ? "fas fa-star": raiting>=3.5 ? "fas fa-star-half"  : "far fa-star"} ></i>
            <i style ={{color: "yellow"}} className= { raiting >= 5 ? "fas fa-star": raiting>=4.5 ? "fas fa-star-half"  : "far fa-star"} ></i>
 
-           
-            {/* <i className="fas fa-star" ></i>
-            <i className="far fa-star"></i>
-            <i className="fas fa-star-half"></i> */}
         </div>
     )
 }
