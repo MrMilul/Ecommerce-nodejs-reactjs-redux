0b04711bbb84e24e0dc119ed88c38105f6baf704
mrmilul <miladmizani@yahoo.com>
2021-06-23 19:46:47 +0300

    add to cart action

---
+++ client/src/redux/actions/Cart.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import CART_ADD_ITEM from '../types/CartType'
+
+
+
+export const addToCart = (productId, Qty)=> async(dispatch, getState){
+
+    const { data } = await axios.get(`/api/products/${productId}`)
+
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload:{
+            name: data.name,
+            price: data.peice,
+            img: data.img,
+            countInStack: data.countInStock,
+            product: data._id,
+            Qty
+        }
+    })
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
