caa3f480b162dfbf2f497379f736bf87d2e9b1fa
mrmilul <miladmizani@yahoo.com>
2021-06-25 11:37:48 +0300

    modified

--- client/src/components/screens/Cart.js
+++ client/src/components/screens/Cart.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../../redux/actions/Cart'
+
 
  const Cart = (props) => {
-
+    const productId = props.match.params.id;
+    const Qty = props.location.search ? props.location.search.split("=")[1] : 1
+    const dispatch = useDispatch()
+    useEffect(() => {
+       dispatch(addToCart(productId, Qty))
+    }, [dispatch,productId, Qty])
     return (
         <div>
             qty : {props.location.search.split("=")[1]} <br/>
--- client/src/redux/types/CartType.js
+++ client/src/redux/types/CartType.js
@@ -1,1 +1,1 @@
-const CART_ADD_ITEM = "CART_ADD_ITEM"
+export const CART_ADD_ITEM = "CART_ADD_ITEM"
