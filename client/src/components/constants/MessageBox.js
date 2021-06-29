addc194d7a80cfb71aa1d02a0cfa9066ccaba6bf
mrmilul <miladmizani@yahoo.com>
2021-06-29 19:00:40 +0300

    modified

--- client/src/components/constants/MessageBox.js
+++ client/src/components/constants/MessageBox.js
@@ -4,6 +4,7 @@ export default function MessageBox(props) {
     return (
         <div className={`alert alert-${props.status} alert-dismissible fade show`} role="alert">
         <strong>Holy guacamole!</strong> {props.message}
+        <div>{props.children}</div>
         <button type="button" className="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
