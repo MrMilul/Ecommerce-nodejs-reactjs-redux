32090dbe8b350f84d52abc1fab1966c262722d7d
mrmilul <miladmizani@yahoo.com>
2021-07-05 19:30:57 +0300

    add user to index reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import {productsListReducer, 
     productDetail} from './products'
 import { cartReducer } from './cart'
+import { userReducer } from './user'
 
 
 const rootReducer = combineReducers({
     productList: productsListReducer,
     productDetail,
-    cart: cartReducer
+    cart: cartReducer,
+    userInfo: userReducer
 })
 
 
