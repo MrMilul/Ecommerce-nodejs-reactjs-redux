b6d29a66978b2519c2bc4ff2eb94dd0b1d692ddb
mrmilul <miladmizani@yahoo.com>
2021-07-07 19:50:40 +0300

    signIn reducer coded

--- client/src/redux/reducers/user.js
+++ client/src/redux/reducers/user.js
@@ -1,10 +1,14 @@
 
 import { USER_REGISTERATION_REQUEST, 
     USER_REGISTERATION_SUCCESS, 
-    USER_REGISTERATION_FAIL} from '../types/RegisterationType'
+    USER_REGISTERATION_FAIL, 
+    USER_SIGNIN_REQUEST,
+    USER_SIGNIN_SUCCESS,
+    USER_SIGNIN_FAIL, 
+    USER_SIGNOUT} from '../types/RegisterationType'
 
 
-export const userReducer = (state={loading:false, user:{}}, action)=>{
+export const userRegisterReducer = (state={}, action)=>{
     switch(action.type){
         case USER_REGISTERATION_REQUEST:
             return{
@@ -13,13 +17,36 @@ export const userReducer = (state={loading:false, user:{}}, action)=>{
         case USER_REGISTERATION_SUCCESS:
             return{
                 loading:false,
-                user: action.payload
+                userInfo: action.payload
             }
         case USER_REGISTERATION_FAIL:
             return {
                 loading:false,
                 error: action.payload
             }
+        default: return state
+    }
+}
+
+
+export const userSignInReducer = (state={}, action)=>{
+    switch(action.type){
+        case USER_SIGNIN_REQUEST:
+            return{
+                loading:true
+            }
+        case USER_SIGNIN_SUCCESS:
+            return{
+                loading:false,
+                userInfo: action.payload
+            }
+        case USER_SIGNIN_FAIL:
+            return {
+                loading:false,
+                error: action.payload
+            }
+        case USER_SIGNOUT: 
+            return{}
         default: return state
     }
 }
