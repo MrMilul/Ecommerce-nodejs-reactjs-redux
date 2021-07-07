99a51d38c9f9629cbf1ab766f7fc2f59a6b2899b
mrmilul <miladmizani@yahoo.com>
2021-07-07 19:48:49 +0300

    functionality coded

--- client/src/components/screens/user/SignIn.js
+++ client/src/components/screens/user/SignIn.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
 import {Link} from 'react-router-dom'
+
+import { userSignIn } from '../../../redux/actions/User'
+
  const SignIn = () => {
+
+
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const dispatch = useDispatch()
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+
+        dispatch(userSignIn(email, password))
+
+    }
+    
+
     return (
         <div className="container mt-5">
         <div className="row">
+
                 <div className="col"></div>
 
 
                 <div className="col border">
 
-                    <form className="m-5">
+                    <form className="m-5" onSubmit={submitHandler}>
                        
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                            <input type="email" 
+                            className="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp"
+                            onChange={(e)=> setEmail(e.target.value)}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"/>
+                            <input type="password" 
+                            className="form-control" 
+                            id="exampleInputPassword1"
+                            onChange={(e)=> setPassword(e.target.value)}/>
                         </div>
                         
                         <button type="submit" className="btn btn-primary">Submit</button>
