eb493429a7f66fbaa075988c86f90000b4fc93b2
mrmilul <miladmizani@yahoo.com>
2021-08-12 20:50:43 +0300

    shipping address reducer

--- client/src/redux/reducers/cart.js
+++ client/src/redux/reducers/cart.js
@@ -1,4 +1,4 @@
-import{ CART_ADD_ITEM, CART_REMOVE_ITEM} from '../types/CartType'
+import{ CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../types/CartType'
 
 
 export const cartReducer = (state={cartItems:[]}, action)=>{
@@ -26,7 +26,12 @@ export const cartReducer = (state={cartItems:[]}, action)=>{
                 ...state,
                 cartItems : state.cartItems.filter((x)=> x.product !== action.payload)
             }
-
+        
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return{
+                ...state,
+                shippingAddress: action.payload
+            }
         default: return state
     }
 }
