aca644b065eeadcdbc408423babab34e437e6cba
mrmilul <miladmizani@yahoo.com>
2021-08-12 20:50:09 +0300

    shipping is added to the localStorage

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -4,17 +4,21 @@ import rootReducer from './reducers'
 
 
 const initialState = {
-   cart:{
-        cartItems: localStorage.getItem('cartItems') 
-        ? JSON.parse(localStorage.getItem('cartItems'))
-        : []
-   }, 
-   userSignIn:{
-           userInfo: localStorage.getItem('userInfo')
-           ?JSON.parse(localStorage.getItem('userInfo'))
-           : null
-   }
-  };
+        cart: {
+                cartItems: localStorage.getItem('cartItems')
+                        ? JSON.parse(localStorage.getItem('cartItems'))
+                        : [],
+                shippingAddres: localStorage.getItem('shppingAddress')
+                ? localStorage.getItem('shppingAddress')
+                : {}
+        },
+        userSignIn: {
+                userInfo: localStorage.getItem('userInfo')
+                        ? JSON.parse(localStorage.getItem('userInfo'))
+                        : null
+        },
+        
+};
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
