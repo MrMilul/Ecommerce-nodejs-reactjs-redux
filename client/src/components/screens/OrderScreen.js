0f17cbbfce824cfa04a70f6d757f05d55bb231fc
mrmilul <miladmizani@yahoo.com>
2021-08-30 21:27:56 +0300

    action is implemented

--- client/src/components/screens/OrderScreen.js
+++ client/src/components/screens/OrderScreen.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { orderDetail } from '../../redux/actions/Order'
+
 
 const OrderScreen = (props) => {
+    const orderId = props.match.params.id
+
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(orderDetail(orderId))
+    }, orderId, dispatch)
+
+    const orderDetails = useSelector(state => state.orderDetail)
+    const { loading, error, order } = orderDetails
+
     return (
         <div>
             {props.match.params.id}
