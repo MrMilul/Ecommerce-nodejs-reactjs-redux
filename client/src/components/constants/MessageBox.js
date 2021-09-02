84a8dc2ce0ebca6873636bea88a09caa5b6224b5
mrmilul <miladmizani@yahoo.com>
2021-09-02 21:18:43 +0300

    order screen

--- client/src/components/constants/MessageBox.js
+++ client/src/components/constants/MessageBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MessageBox(props) {
     return (
         <div className={`alert alert-${props.status} alert-dismissible fade show`} role="alert">
-        <strong>Holy guacamole!</strong> {props.message}
+       {props.status ==  "danger" ? <strong>Holy guacamole!</strong>  : ''}  {props.message}
         <div>{props.children}</div>
         <button type="button" className="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
--- client/src/components/screens/OrderScreen.js
+++ client/src/components/screens/OrderScreen.js
@@ -1,23 +1,93 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import { orderDetail } from '../../redux/actions/Order'
 
 
+import LoadingBox from "../constants/LoadingBox"
+import MessageBox from "../constants/MessageBox"
 const OrderScreen = (props) => {
     const orderId = props.match.params.id
 
     const dispatch = useDispatch()
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(orderDetail(orderId))
-    }, orderId, dispatch)
+    }, [orderId, dispatch])
 
     const orderDetails = useSelector(state => state.orderDetail)
     const { loading, error, order } = orderDetails
 
     return (
         <div>
-            {props.match.params.id}
+            {
+                loading ? (<div className="container mt-5"><LoadingBox /></div>)
+                    : error ? (<div className="container mt-5"><MessageBox status="success">{error}</MessageBox></div>)
+                        : (
+
+                            <div className="row m-4">
+                                {
+
+
+                                    <div className=" col-md-8 col-sm-12 ">
+                                        <p>Order ID: {order._id}</p>
+                                        <div className="card mt-4">
+                                            <div className="card-header bg-success">
+                                                Shippingg
+                                            </div>
+                                            <div className="card-body">
+                                                <h4>Name:</h4> {order.shippingAddress.fullName}
+                                                <h4>Address:</h4>{order.shippingAddress.address}, {order.shippingAddress.city}, {order.shippingAddress.postalCode}, {order.shippingAddress.country}
+
+                                                <MessageBox status="danger">Not Delivered</MessageBox>
+
+                                            </div>
+                                        </div>
+
+
+
+                                        <div className="card mt-4">
+                                            <div className="card-header bg-success">
+                                                Payment
+                                            </div>
+                                            <div className="card-body">
+                                                <h4>Payment Method:</h4> {order.paymentMethod}
+
+                                                {
+                                                    order.isPaid
+                                                        ? <MessageBox status="succuss">Paid</MessageBox>
+                                                        : <MessageBox status="danger">Not Paid</MessageBox>
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                }
+
+
+
+
+                                <div className="col-md-4 col-sm-12 ">
+                                    <div className="card mt-5">
+                                        <div className="card-header">
+                                            Quote
+                                        </div>
+                                        <div className="card-body">
+                                            <div>Items:</div>
+                                            <div>Shipping: </div>
+                                            <div>Tax: </div>
+                                            <div className="font-weight-bold">Order Total: </div>
+
+                                            <button className="btn btn-block btn-success mt-5">PayPal</button>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        )
+            }
         </div>
     )
 }
