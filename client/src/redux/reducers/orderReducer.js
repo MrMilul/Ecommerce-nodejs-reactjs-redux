69be4a01900587bcfd43aad92fbc849d7e284a5d
mrmilul <miladmizani@yahoo.com>
2021-09-27 19:56:43 +0300

    order history reducer

--- client/src/redux/reducers/orderReducer.js
+++ client/src/redux/reducers/orderReducer.js
@@ -9,7 +9,10 @@ import {
     ORDER_PAY_REQUEST,
     ORDER_PAY_SUCCESS,
     ORDER_PAY_RESET,
-    ORDER_PAY_FAIL
+    ORDER_PAY_FAIL,
+    ORDER_MINE_LIST_FAIL,
+    ORDER_MINE_LIST_REQUEST,
+    ORDER_MINE_LIST_SUCCESS
 } from "../types/OrderTypes"
 
 
@@ -67,10 +70,32 @@ export const payOrder = (state = {}, action) => {
         case ORDER_PAY_SUCCESS:
             return { loading: false, success: true }
         case ORDER_PAY_FAIL:
-            return {loading:false, error:action.payload}
+            return { loading: false, error: action.payload }
         case ORDER_PAY_RESET:
             return {}
         default: return state
     }
 
+}
+
+
+export const orderMineListReducer = (state = { order: [] }, action) => {
+    switch (action.type) {
+        case ORDER_MINE_LIST_REQUEST:
+            return { loading: true, userInfo:action.payload }
+
+        case ORDER_MINE_LIST_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload
+            }
+
+        case ORDER_MINE_LIST_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        default: return state
+    }
 }
